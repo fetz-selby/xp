525dfec8c740dc267f5af7234ef1211c6ace85a3
Emmanuel Selby <fetz.selby@gmail.com>
2025-03-12 16:37:33 +0100

    fix url

--- utils/api.ts
+++ utils/api.ts
@@ -1,24 +1,20 @@
+const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'
+
 export const getLocations = async () => {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/locations`
-  )
+  const response = await fetch(`${apiUrl}/api/locations`)
   const { data } = await response.json()
   return data
 }
 
 export const getXPById = async (xpId: string) => {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/xp/${xpId}`
-  )
+  const response = await fetch(`${apiUrl}/api/xp/${xpId}`)
   const { data } = await response.json()
   return data
 }
 
 export const getXP = async (params: Record<string, string>) => {
   const response = await fetch(
-    new Request(
-      `${process.env.NEXT_PUBLIC_API_URL}/api/xp?${new URLSearchParams(params)}`
-    )
+    new Request(`${apiUrl}/api/xp?${new URLSearchParams(params)}`)
   )
   const { data } = await response.json()
   return data
